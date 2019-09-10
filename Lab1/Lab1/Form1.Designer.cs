3a1273b254537d0062ee809cb901128e3448aa4b
Alexander <AlexanderTurchin1@gmail.com>
2019-09-10 14:54:44 +0300

    Add modulation

--- Lab1/Lab1/Form1.Designer.cs
+++ Lab1/Lab1/Form1.Designer.cs
@@ -40,16 +40,27 @@
             this.lblPhase = new System.Windows.Forms.Label();
             this.lblTime = new System.Windows.Forms.Label();
             this.barTime = new System.Windows.Forms.TrackBar();
-            this.txtAmplitude = new System.Windows.Forms.TextBox();
-            this.txtFrequency = new System.Windows.Forms.TextBox();
-            this.txtPhase = new System.Windows.Forms.TextBox();
-            this.txtTime = new System.Windows.Forms.TextBox();
             this.btnGenerate = new System.Windows.Forms.Button();
+            this.panelValues = new System.Windows.Forms.Panel();
+            this.lblTimeValue = new System.Windows.Forms.Label();
+            this.lblPhaseValue = new System.Windows.Forms.Label();
+            this.lblFrequencyValue = new System.Windows.Forms.Label();
+            this.lblAmplitudeValue = new System.Windows.Forms.Label();
+            this.cmbSignals = new System.Windows.Forms.ComboBox();
+            this.lboxSignals = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnModulate = new System.Windows.Forms.Button();
+            this.lblDutyCycleValue = new System.Windows.Forms.Label();
+            this.lblDutyCycle = new System.Windows.Forms.Label();
+            this.barDutyCycle = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.barAmplitude)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.barFrequency)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.barPhase)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.barTime)).BeginInit();
+            this.panelValues.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barDutyCycle)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
@@ -69,46 +80,49 @@
             series1.Name = "Series1";
             series1.YValuesPerPoint = 2;
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(837, 300);
+            this.chart1.Size = new System.Drawing.Size(742, 468);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
             // barAmplitude
             // 
+            this.barAmplitude.Enabled = false;
             this.barAmplitude.LargeChange = 1;
-            this.barAmplitude.Location = new System.Drawing.Point(98, 400);
+            this.barAmplitude.Location = new System.Drawing.Point(62, 14);
             this.barAmplitude.Minimum = 1;
             this.barAmplitude.Name = "barAmplitude";
-            this.barAmplitude.Size = new System.Drawing.Size(798, 45);
+            this.barAmplitude.Size = new System.Drawing.Size(129, 45);
             this.barAmplitude.TabIndex = 1;
             this.barAmplitude.Value = 1;
             this.barAmplitude.Scroll += new System.EventHandler(this.BarAmplitude_Scroll);
             // 
             // barFrequency
             // 
+            this.barFrequency.Enabled = false;
             this.barFrequency.LargeChange = 1;
-            this.barFrequency.Location = new System.Drawing.Point(98, 451);
+            this.barFrequency.Location = new System.Drawing.Point(62, 65);
             this.barFrequency.Minimum = 1;
             this.barFrequency.Name = "barFrequency";
-            this.barFrequency.Size = new System.Drawing.Size(798, 45);
+            this.barFrequency.Size = new System.Drawing.Size(129, 45);
             this.barFrequency.TabIndex = 2;
             this.barFrequency.Value = 1;
             this.barFrequency.Scroll += new System.EventHandler(this.BarFrequency_Scroll);
             // 
             // barPhase
             // 
+            this.barPhase.Enabled = false;
             this.barPhase.LargeChange = 1;
-            this.barPhase.Location = new System.Drawing.Point(98, 502);
+            this.barPhase.Location = new System.Drawing.Point(62, 116);
             this.barPhase.Maximum = 4;
             this.barPhase.Name = "barPhase";
-            this.barPhase.Size = new System.Drawing.Size(798, 45);
+            this.barPhase.Size = new System.Drawing.Size(129, 45);
             this.barPhase.TabIndex = 3;
             this.barPhase.Scroll += new System.EventHandler(this.BarPhase_Scroll);
             // 
             // lblAmplitude
             // 
             this.lblAmplitude.AutoSize = true;
-            this.lblAmplitude.Location = new System.Drawing.Point(0, 400);
+            this.lblAmplitude.Location = new System.Drawing.Point(3, 14);
             this.lblAmplitude.Name = "lblAmplitude";
             this.lblAmplitude.Size = new System.Drawing.Size(53, 13);
             this.lblAmplitude.TabIndex = 4;
@@ -117,7 +131,7 @@
             // lblFrequency
             // 
             this.lblFrequency.AutoSize = true;
-            this.lblFrequency.Location = new System.Drawing.Point(0, 451);
+            this.lblFrequency.Location = new System.Drawing.Point(3, 65);
             this.lblFrequency.Name = "lblFrequency";
             this.lblFrequency.Size = new System.Drawing.Size(57, 13);
             this.lblFrequency.TabIndex = 5;
@@ -126,7 +140,7 @@
             // lblPhase
             // 
             this.lblPhase.AutoSize = true;
-            this.lblPhase.Location = new System.Drawing.Point(-3, 502);
+            this.lblPhase.Location = new System.Drawing.Point(3, 116);
             this.lblPhase.Name = "lblPhase";
             this.lblPhase.Size = new System.Drawing.Size(37, 13);
             this.lblPhase.TabIndex = 6;
@@ -135,7 +149,7 @@
             // lblTime
             // 
             this.lblTime.AutoSize = true;
-            this.lblTime.Location = new System.Drawing.Point(-3, 553);
+            this.lblTime.Location = new System.Drawing.Point(3, 167);
             this.lblTime.Name = "lblTime";
             this.lblTime.Size = new System.Drawing.Size(30, 13);
             this.lblTime.TabIndex = 8;
@@ -143,51 +157,20 @@
             // 
             // barTime
             // 
+            this.barTime.Enabled = false;
             this.barTime.LargeChange = 1;
-            this.barTime.Location = new System.Drawing.Point(98, 553);
+            this.barTime.Location = new System.Drawing.Point(62, 167);
             this.barTime.Maximum = 3;
             this.barTime.Minimum = 1;
             this.barTime.Name = "barTime";
-            this.barTime.Size = new System.Drawing.Size(798, 45);
+            this.barTime.Size = new System.Drawing.Size(129, 45);
             this.barTime.TabIndex = 7;
             this.barTime.Value = 1;
             this.barTime.Scroll += new System.EventHandler(this.BarTime_Scroll);
             // 
-            // txtAmplitude
-            // 
-            this.txtAmplitude.Location = new System.Drawing.Point(914, 400);
-            this.txtAmplitude.Name = "txtAmplitude";
-            this.txtAmplitude.ReadOnly = true;
-            this.txtAmplitude.Size = new System.Drawing.Size(58, 20);
-            this.txtAmplitude.TabIndex = 9;
-            // 
-            // txtFrequency
-            // 
-            this.txtFrequency.Location = new System.Drawing.Point(914, 448);
-            this.txtFrequency.Name = "txtFrequency";
-            this.txtFrequency.ReadOnly = true;
-            this.txtFrequency.Size = new System.Drawing.Size(58, 20);
-            this.txtFrequency.TabIndex = 10;
-            // 
-            // txtPhase
-            // 
-            this.txtPhase.Location = new System.Drawing.Point(914, 499);
-            this.txtPhase.Name = "txtPhase";
-            this.txtPhase.ReadOnly = true;
-            this.txtPhase.Size = new System.Drawing.Size(58, 20);
-            this.txtPhase.TabIndex = 11;
-            // 
-            // txtTime
-            // 
-            this.txtTime.Location = new System.Drawing.Point(914, 546);
-            this.txtTime.Name = "txtTime";
-            this.txtTime.ReadOnly = true;
-            this.txtTime.Size = new System.Drawing.Size(58, 20);
-            this.txtTime.TabIndex = 12;
-            // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(880, 88);
+            this.btnGenerate.Location = new System.Drawing.Point(888, 442);
             this.btnGenerate.Name = "btnGenerate";
             this.btnGenerate.Size = new System.Drawing.Size(75, 23);
             this.btnGenerate.TabIndex = 13;
@@ -195,24 +178,158 @@
             this.btnGenerate.UseVisualStyleBackColor = true;
             this.btnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
             // 
+            // panelValues
+            // 
+            this.panelValues.Controls.Add(this.lblDutyCycleValue);
+            this.panelValues.Controls.Add(this.lblDutyCycle);
+            this.panelValues.Controls.Add(this.barDutyCycle);
+            this.panelValues.Controls.Add(this.lblTimeValue);
+            this.panelValues.Controls.Add(this.lblPhaseValue);
+            this.panelValues.Controls.Add(this.lblFrequencyValue);
+            this.panelValues.Controls.Add(this.lblAmplitudeValue);
+            this.panelValues.Controls.Add(this.lblAmplitude);
+            this.panelValues.Controls.Add(this.barAmplitude);
+            this.panelValues.Controls.Add(this.lblTime);
+            this.panelValues.Controls.Add(this.barFrequency);
+            this.panelValues.Controls.Add(this.barTime);
+            this.panelValues.Controls.Add(this.barPhase);
+            this.panelValues.Controls.Add(this.lblPhase);
+            this.panelValues.Controls.Add(this.lblFrequency);
+            this.panelValues.Location = new System.Drawing.Point(764, 152);
+            this.panelValues.Name = "panelValues";
+            this.panelValues.Size = new System.Drawing.Size(200, 275);
+            this.panelValues.TabIndex = 14;
+            // 
+            // lblTimeValue
+            // 
+            this.lblTimeValue.AutoSize = true;
+            this.lblTimeValue.Location = new System.Drawing.Point(3, 180);
+            this.lblTimeValue.Name = "lblTimeValue";
+            this.lblTimeValue.Size = new System.Drawing.Size(41, 13);
+            this.lblTimeValue.TabIndex = 12;
+            this.lblTimeValue.Text = "PValue";
+            // 
+            // lblPhaseValue
+            // 
+            this.lblPhaseValue.AutoSize = true;
+            this.lblPhaseValue.Location = new System.Drawing.Point(3, 134);
+            this.lblPhaseValue.Name = "lblPhaseValue";
+            this.lblPhaseValue.Size = new System.Drawing.Size(41, 13);
+            this.lblPhaseValue.TabIndex = 11;
+            this.lblPhaseValue.Text = "PValue";
+            // 
+            // lblFrequencyValue
+            // 
+            this.lblFrequencyValue.AutoSize = true;
+            this.lblFrequencyValue.Location = new System.Drawing.Point(3, 82);
+            this.lblFrequencyValue.Name = "lblFrequencyValue";
+            this.lblFrequencyValue.Size = new System.Drawing.Size(40, 13);
+            this.lblFrequencyValue.TabIndex = 10;
+            this.lblFrequencyValue.Text = "FValue";
+            // 
+            // lblAmplitudeValue
+            // 
+            this.lblAmplitudeValue.AutoSize = true;
+            this.lblAmplitudeValue.Location = new System.Drawing.Point(3, 34);
+            this.lblAmplitudeValue.Name = "lblAmplitudeValue";
+            this.lblAmplitudeValue.Size = new System.Drawing.Size(41, 13);
+            this.lblAmplitudeValue.TabIndex = 9;
+            this.lblAmplitudeValue.Text = "AValue";
+            // 
+            // cmbSignals
+            // 
+            this.cmbSignals.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSignals.FormattingEnabled = true;
+            this.cmbSignals.Items.AddRange(new object[] {
+            "Sinus",
+            "Impulse",
+            "Triangle",
+            "Sawtooth"});
+            this.cmbSignals.Location = new System.Drawing.Point(763, 13);
+            this.cmbSignals.Name = "cmbSignals";
+            this.cmbSignals.Size = new System.Drawing.Size(200, 21);
+            this.cmbSignals.TabIndex = 17;
+            this.cmbSignals.SelectedIndexChanged += new System.EventHandler(this.CmbSignals_SelectedIndexChanged);
+            // 
+            // lboxSignals
+            // 
+            this.lboxSignals.FormattingEnabled = true;
+            this.lboxSignals.Location = new System.Drawing.Point(763, 51);
+            this.lboxSignals.Name = "lboxSignals";
+            this.lboxSignals.Size = new System.Drawing.Size(120, 95);
+            this.lboxSignals.TabIndex = 18;
+            this.lboxSignals.SelectedIndexChanged += new System.EventHandler(this.LboxSignals_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(889, 51);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 19;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(889, 80);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // btnModulate
+            // 
+            this.btnModulate.Location = new System.Drawing.Point(785, 442);
+            this.btnModulate.Name = "btnModulate";
+            this.btnModulate.Size = new System.Drawing.Size(75, 23);
+            this.btnModulate.TabIndex = 21;
+            this.btnModulate.Text = "Modulate";
+            this.btnModulate.UseVisualStyleBackColor = true;
+            this.btnModulate.Click += new System.EventHandler(this.BtnModulate_Click);
+            // 
+            // lblDutyCycleValue
+            // 
+            this.lblDutyCycleValue.AutoSize = true;
+            this.lblDutyCycleValue.Location = new System.Drawing.Point(3, 221);
+            this.lblDutyCycleValue.Name = "lblDutyCycleValue";
+            this.lblDutyCycleValue.Size = new System.Drawing.Size(41, 13);
+            this.lblDutyCycleValue.TabIndex = 15;
+            this.lblDutyCycleValue.Text = "PValue";
+            // 
+            // lblDutyCycle
+            // 
+            this.lblDutyCycle.AutoSize = true;
+            this.lblDutyCycle.Location = new System.Drawing.Point(3, 208);
+            this.lblDutyCycle.Name = "lblDutyCycle";
+            this.lblDutyCycle.Size = new System.Drawing.Size(55, 13);
+            this.lblDutyCycle.TabIndex = 14;
+            this.lblDutyCycle.Text = "DutyCycle";
+            // 
+            // barDutyCycle
+            // 
+            this.barDutyCycle.Enabled = false;
+            this.barDutyCycle.LargeChange = 1;
+            this.barDutyCycle.Location = new System.Drawing.Point(62, 208);
+            this.barDutyCycle.Name = "barDutyCycle";
+            this.barDutyCycle.Size = new System.Drawing.Size(129, 45);
+            this.barDutyCycle.TabIndex = 13;
+            this.barDutyCycle.Value = 1;
+            // 
             // Lab1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 755);
+            this.ClientSize = new System.Drawing.Size(984, 477);
+            this.Controls.Add(this.btnModulate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lboxSignals);
+            this.Controls.Add(this.cmbSignals);
+            this.Controls.Add(this.panelValues);
             this.Controls.Add(this.btnGenerate);
-            this.Controls.Add(this.txtTime);
-            this.Controls.Add(this.txtPhase);
-            this.Controls.Add(this.txtFrequency);
-            this.Controls.Add(this.txtAmplitude);
-            this.Controls.Add(this.lblTime);
-            this.Controls.Add(this.barTime);
-            this.Controls.Add(this.lblPhase);
-            this.Controls.Add(this.lblFrequency);
-            this.Controls.Add(this.lblAmplitude);
-            this.Controls.Add(this.barPhase);
-            this.Controls.Add(this.barFrequency);
-            this.Controls.Add(this.barAmplitude);
             this.Controls.Add(this.chart1);
             this.Name = "Lab1";
             this.Text = "Lab work 1";
@@ -221,8 +338,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.barFrequency)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.barPhase)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.barTime)).EndInit();
+            this.panelValues.ResumeLayout(false);
+            this.panelValues.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barDutyCycle)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -237,11 +356,20 @@
         private System.Windows.Forms.Label lblPhase;
         private System.Windows.Forms.Label lblTime;
         private System.Windows.Forms.TrackBar barTime;
-        private System.Windows.Forms.TextBox txtAmplitude;
-        private System.Windows.Forms.TextBox txtFrequency;
-        private System.Windows.Forms.TextBox txtPhase;
-        private System.Windows.Forms.TextBox txtTime;
         private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Panel panelValues;
+        private System.Windows.Forms.ComboBox cmbSignals;
+        private System.Windows.Forms.Label lblTimeValue;
+        private System.Windows.Forms.Label lblPhaseValue;
+        private System.Windows.Forms.Label lblFrequencyValue;
+        private System.Windows.Forms.Label lblAmplitudeValue;
+        private System.Windows.Forms.ListBox lboxSignals;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnModulate;
+        private System.Windows.Forms.Label lblDutyCycleValue;
+        private System.Windows.Forms.Label lblDutyCycle;
+        private System.Windows.Forms.TrackBar barDutyCycle;
     }
 }
 
--- Lab1/Lab1/Form1.cs
+++ Lab1/Lab1/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using Signals;
@@ -16,57 +10,77 @@ namespace Lab1
     {
         private static double PI = Math.PI;
 
-        private readonly double[] phases = {0, PI / 6, PI / 4, PI / 2, PI};
-        private readonly string[] phasesText = {"0", "π/6", "π/4", "π/2", "π"};
-
+        private readonly double[] Phases = {0, PI / 6, PI / 4, PI / 2, PI};
+        private readonly string[] PhasesText = {"0", "π/6", "π/4", "π/2", "π"};
+        private const double DutyFactor = 0.95;
         private Data data;
+
+        private List<Data> datas = new List<Data>();
+        private List<BaseSignal> signals = new List<BaseSignal>();
+
+        private BaseSignal signal = new SinSignal();
+
 
         public Lab1()
         {
             InitializeComponent();
-            ShowBarValue(barAmplitude, txtAmplitude, out data.amplitude);
-            ShowBarValue(barFrequency, txtFrequency, out data.frequency);
-            ShowBarValue(barTime, txtTime, out data.time);
-            ShowBarPhaseValue(barPhase, txtPhase, out data.startPhase);
-
-            data.samplingFrequency = 800;
 
             ConfigureAxis(chart1.ChartAreas[0].AxisX);
             ConfigureAxis(chart1.ChartAreas[0].AxisY);
 
             chart1.ChartAreas[0].AxisY.Maximum = 10;
 
+            cmbSignals.SelectedIndex = 0;
         }
 
         #region ControlEvents
 
         private void BarAmplitude_Scroll(object sender, EventArgs e)
         {
-            ShowBarValue(sender, txtAmplitude, out data.amplitude);
+            ShowBarValue(sender, lblAmplitudeValue, out data.amplitude);
         }
 
         private void BarFrequency_Scroll(object sender, EventArgs e)
         {
-            ShowBarValue(sender, txtFrequency, out data.frequency);
+            ShowBarValue(sender, lblFrequencyValue, out data.frequency);
         }
 
         private void BarTime_Scroll(object sender, EventArgs e)
         {
-            ShowBarValue(sender, txtTime, out data.time);
+            ShowBarValue(sender, lblTimeValue, out data.time);
         }
 
         private void BarPhase_Scroll(object sender, EventArgs e)
         {
-            ShowBarPhaseValue(sender, txtPhase, out data.startPhase);
+            ShowBarPhaseValue(sender, lblPhaseValue, out data.startPhase);
         }
 
         private void BtnGenerate_Click(object sender, EventArgs e)
         {
-            BaseSignal signal = new ImpulseSignal(0.5);
-
-            var results = signal.GetResultValues(data);
-
+            int index = lboxSignals.SelectedIndex;
+            var results = signals[index].GetResultValues(datas[index]);
             CreateChartFunction(results);
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            signals.Add(signal);
+            datas.Add(new Data());
+
+            lboxSignals.Items.Add(signal.ToString());
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            int index = lboxSignals.SelectedIndex;
+
+            if (index >= 0)
+            {
+                signals.RemoveAt(index);
+                datas.RemoveAt(index);
+                lboxSignals.Items.RemoveAt(index);
+                lboxSignals.SelectedIndex = index - 1;
+            }
         }
 
         #endregion
@@ -78,7 +92,7 @@ namespace Lab1
             axis.Crossing = 0;
         }
 
-        private void ShowBarPhaseValue(object trackBar, TextBox textBox, out double value)
+        private void ShowBarPhaseValue(object trackBar, object label, out double value)
         {
             var bar = trackBar as TrackBar;
             if (bar == null)
@@ -86,22 +100,22 @@ namespace Lab1
                 throw new ArgumentException(nameof(trackBar));
             }
 
-            var text = textBox as TextBox;
-            if (text == null)
+            var lbl = label as Label;
+            if (lbl == null)
             {
-                throw new ArgumentException(nameof(textBox));
+                throw new ArgumentException(nameof(label));
             }
 
-            if (bar.Value >= phases.Length)
+            if (bar.Value >= Phases.Length)
             {
                 throw new ArgumentOutOfRangeException(nameof(bar.Value));
             }
 
-            text.Text = phasesText[bar.Value];
-            value = phases[bar.Value];
+            lbl.Text = PhasesText[bar.Value];
+            value = Phases[bar.Value];
         }
 
-        private void ShowBarValue(object trackBar, object textBox, out int value)
+        private void ShowBarValue(object trackBar, object label, out int value)
         {
             var bar = trackBar as TrackBar;
             if (bar == null)
@@ -109,28 +123,118 @@ namespace Lab1
                 throw new ArgumentException(nameof(trackBar));
             }
 
-            var text = textBox as TextBox;
-            if (text == null)
+            var lbl = label as Label;
+            if (lbl == null)
             {
-                throw new ArgumentException(nameof(textBox));
+                throw new ArgumentException(nameof(label));
             }
 
             value = bar.Value;
-            text.Text = bar.Value.ToString();
+            lbl.Text = bar.Value.ToString();
             
         }
         
-        private void CreateChartFunction(double[][] results)
+        private void CreateChartFunction(Point[] points)
         {
             chart1.Series[0].Points.Clear();
             //chart1.Series.Add("Series1");
 
-            foreach (var point in results)
+            foreach (var point in points)
             {
-                chart1.Series[0].Points.AddXY(point[0], point[1]);
+                chart1.Series[0].Points.AddXY(point.X, point.Y);
+            }
+        }
+
+        private void CmbSignals_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            if (comboBox == null)
+            {
+                throw new NullReferenceException(nameof(sender));
             }
 
+            switch (comboBox.SelectedIndex)
+            {
+                case 0:
+                    signal = new SinSignal();
+                    break;
+                case 1:
+                    signal = new ImpulseSignal();
+                    break;
+                case 2:
+                    signal = new TriangleSignal();
+                    break;
+                case 3:
+                    signal = new SawtoothSignal();
+                    break;
+            }
+        }
+
+        private void BtnModulate_Click(object sender, EventArgs e)
+        {
+            CreateModulateChart();
+        }
+
+        private void CreateModulateChart()
+        {
+            Point[][] funcResults = new Point[signals.Count][];
             
+
+            for(int i = 0; i < funcResults.Length; i++)
+            {
+                funcResults[i] = signals[i].GetResultValues(datas[i]);
+            }
+
+            Point[] result = new Point[funcResults[0].Length];
+
+            for (int i = 0; i < funcResults[0].Length; i++)
+            {
+                result[i].X = funcResults[0][i].X;
+
+                for (int y = 0; y < funcResults.Length; y++)
+                {
+                    result[i].Y += funcResults[y][i].Y;
+                }
+            }
+            CreateChartFunction(result);
+        }
+
+        private void LboxSignals_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bool enabled = lboxSignals.SelectedIndex >= 0;
+
+            
+            barAmplitude.Enabled = enabled;
+            barFrequency.Enabled = enabled;
+            barTime.Enabled = enabled;
+            barDutyCycle.Enabled = enabled;
+
+            if(enabled
+                && lboxSignals.SelectedIndex < datas.Count)
+            {
+                data = datas[lboxSignals.SelectedIndex];
+            }
+            else
+            {
+                data = null;
+                return;
+            }
+
+            barAmplitude.Value = data.amplitude;
+            lblAmplitudeValue.Text = data.amplitude.ToString();
+
+            barFrequency.Value = data.frequency;
+            lblFrequencyValue.Text = data.frequency.ToString();
+
+            //barPhase.Enabled = enabled;
+            //barPhase.Value = data.p;
+            //lblAmplitudeValue.Text = data.amplitude.ToString();
+
+            barTime.Value = data.time;
+            lblTimeValue.Text = data.time.ToString();
+
+            barDutyCycle.Value = (int)(data.dutyFactor * 10);
+            lblDutyCycleValue.Text = barDutyCycle.Value.ToString();
         }
     }
 }
